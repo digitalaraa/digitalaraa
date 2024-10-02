0bea492dea7ea93e4f26d6e720bf211b5e9b8dfc
sarandevarjun <saransri13@gmail.com>
2024-10-02 12:44:42 +0530

    fixed the header file changes

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+import Header from "./Header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
